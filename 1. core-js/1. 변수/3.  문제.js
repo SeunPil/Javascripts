c0e96d23a8d8190d1d027222681d45c8d3ad40a0
SeunPil <cushion2@naver.com>
2021-06-29 11:52:55 +0900

    [21.06.29] # Javascripts 1

---
+++ 1. core-js/1. 변수/3.  문제.js
@@ -0,0 +1,11 @@
+var admin;
+var myName;
+// var admin, myName * 대소문자 조심
+myName = 'John';
+admin = myName;
+console.log(admin);
+
+var OurLivingPlant = 'earth';
+var currentUserName;
+var loginUserName;
+
