a05d7d3bc4f19551fd014f204c89457bd57267cc
SeunPil <cushion2@naver.com>
2021-06-30 11:51:17 +0900

    [21.06.30] # JS 연산자

---
+++ 1. core-js/2. 데이터 타입/3. etc.js
@@ -0,0 +1,19 @@
+
+//논리 (boolean)
+var logic = 10 > 8; // 참
+console.log(typeof logic); // boolean
+console.log(logic); // true
+
+// JS = 소문자 true / false로 boolean 정의 가능
+
+// logic = True; = 단어 True
+// logic = 'false' = 단어 false
+
+//undefined 
+var apple;
+console.log(apple);
+
+//null : 개발자가 명시적으로 없는 상태를 표현할 때
+var banana = null;
+console.log(typeof banana);
+
