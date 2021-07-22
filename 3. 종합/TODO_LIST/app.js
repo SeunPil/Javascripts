0b93713e0a5f37ebda29bb3b82be0dd5ff377ccb
SeunPil <cushion2@naver.com>
2021-07-22 12:03:36 +0900

    [21.07.22] # TODO_LIST 2

--- 3. 종합/TODO_LIST/app.js
+++ 3. 종합/TODO_LIST/app.js
@@ -1,7 +1,5 @@
-
 //전역변수
-const todos = [
-    {
+const todos = [{
         id: 1,
         text: '할 일 1',
         done: false
@@ -23,7 +21,7 @@ const todos = [
 //추가될 새로운 할 일의 신규 아이디를 만들어서 리턴하는 함수
 function makeNewId() {
     if (todos.length > 0) {
-        return todos[todos.length-1].id + 1;
+        return todos[todos.length - 1].id + 1;
     } else { //기존 데이터가 한개도 없는 경우
         return 1;
     }
@@ -31,8 +29,10 @@ function makeNewId() {
 
 //새 할 일을 화면에 렌더링하는 함수
 function renderNewToDo(newToDo) {
+    const $li = document.createElement('li');
+    $li.dataset.id = newToDo.id;
+    $li.classList.add('todo-list-item');
     const newElement = `
-    <li data-id="${newToDo.id}" class="todo-list-item">
         <label class="checkbox">
             <input type="checkbox">
             <span class="text">${newToDo.text}</span>
@@ -41,8 +41,9 @@ function renderNewToDo(newToDo) {
         <div class="remove"><span class="lnr lnr-cross-circle"></span></div>
     </li>
     `;
+    $li.innerHTML = newElement;
     const $toDoList = document.querySelector('.todo-list');
-    $toDoList.innerHTML += newElement;
+    $toDoList.appendChild($li);
 }
 
 //할 일의 내용이 잘 입력되었는지 검사하는 함수
@@ -55,7 +56,7 @@ function validateInputText($targetInput) {
         return false;
     } else {
         $targetInput.style.background = '';
-        $targetInput.setAttribute('placeholder', '할 일을 입력하세요');
+        $targetInput.setAttribute('placeholder', '일정을 입력하세요.');
         return true;
     }
 }
@@ -85,9 +86,77 @@ function insertTodoData() {
     $todoText.value = '';
 }
 
+//data-id값으로 배열을 탐색하여 아이디가 일치하는 객체의 인덱스를 반환
+function findIndexByDataId(dataId) {
+
+    for (let i = 0; i < todos.length; i++) {
+        if (todos[i].id === dataId) {
+            return i;
+        }
+    }
+    return null; // 탐색 실패 증거 null;
+}
+
+
+//할 일 완료 처리 함수 정의
+function changeCheckState($label) {
+    /*
+        1. 체크가 된 label태그에 checked클래스를 추가해야 함.
+        2. 이 함수에는 체크가 된 label태그의 정보가 필요함.
+        3. 이 태그는 이벤트 핸들러가 알고 있음.
+    */
+    $label.classList.toggle('checked');
+
+    /*
+        4. 문제상황: css만 변했지 실제 데이터(done)은 변하지 않음.
+        5. 만약 서버가 있었다면 서버에 완료상태를 반영해줘야 함.
+        6. todos배열의 객체에 접근해서 done값을 변경해줘야 함.
+        7. 만약에 두번째 체크박스를 선택했다면 todos 배열의 todos[1]번 인덱스의
+        done값을 수정 해줘야 함.
+        8. label태그의 정보를 알고 있다면 그 부모태그 li에 접근할 수 있고
+           그 태그의 data-id속성을 조회하면 객체의 id값을 얻을 수 있다.
+    */
+    const dataId = +$label.parentNode.dataset.id; // str이라 숫자로 받아준다.
+
+    // 9. id값을 기반으로 배열을 탐색하여 data-id와 일치하는 
+    // id 프로퍼티를 가진 객체가 있는 인덱스를 찾아야 함.
+    const idx = findIndexByDataId(dataId);
+
+    // 10. 찾은 인덱스로 해당 객체에 접근해서 done값을 수정
+    if (idx !== null) {
+        todos[idx].done = !todos[idx].done;
+    }
+
+}
+
+function removeToDoData($delTarget) {
+    // 1. 삭제 대상 li태그를 ul에서 제거해야함.
+    const $ul = document.querySelector('.todo-list');
+    $ul.removeChild($delTarget);
+
+    // 2. todos배열에서 해당 객체 삭제
+    const delIdx = findIndexByDataId(+$delTarget.dataset.id);
+    todos.splice(delIdx, 1);
+}
+
+function modifyToDoData($modifyTarget) {
+    /*
+        수정 버튼을 눌렀을 때 span이 인풋으로 변경됨. span값이 value로
+    */
+    const $label = $modifyTarget.children[0];
+    console.log($label);
+    const $span = $label.lastElementChild;
+    const $changelabel = document.createElement('input');
+    $changelabel.setAttribute('id', 'mod-input');
+    $changelabel.setAttribute('value', $label.textContent.trim());
+    $label.replaceChild($changelabel, $span);
+}
+
+
+
 
 //메인 실행부
-(function() {
+(function () {
 
     //할 일 추가 이벤트
     const $addBtn = document.getElementById('add');
@@ -99,4 +168,29 @@ function insertTodoData() {
         insertTodoData();
     });
 
-})(); 
+    // 할 일 완료(체크박스) 이벤트
+
+    //$todoList = ul의 클래스를 선택하고 눌렀을 때,
+    const $todoList = document.querySelector('.todo-list');
+    $todoList.addEventListener('change', e => {
+
+        //체크박스에 변화가 생겼을 때 CSS변동 처리 함수 호출
+        // 핸들러의 타겟의 부모 노드
+        changeCheckState(e.target.parentNode);
+    });
+
+    // 할 일 삭제버튼 클릭 이벤트
+    $todoList.addEventListener('click', e => {
+        if (!e.target.matches('.remove span')) return;
+        removeToDoData(e.target.parentNode.parentNode);
+    });
+
+    // 수정 이벤트
+    $todoList.addEventListener('click', e => {
+        if (!e.target.matches('.modify span')) return;
+        modifyToDoData(e.target.parentNode.parentNode);
+    })
+
+
+
+})();
