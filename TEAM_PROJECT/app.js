7f1cf121ddc78f95be9d7d628f08ea44f8f47b9d
SeunPil <cushion2@naver.com>
2021-07-27 11:15:58 +0900

    TPJ # 2

--- TEAM_PROJECT/app.js
+++ TEAM_PROJECT/app.js
@@ -27,7 +27,7 @@ for (let i = 0; colorList.length > 0; i++) {
 
 function makeCard() {
     // Live DOM 생성
-    const $numbers = document.getElementById('numbers');
+    const $numbers = document.querySelector('.container');
 
     // Live DOM에 밀어넣기 전, 가상 DOM 생성
     const $frag = document.createDocumentFragment();
@@ -35,24 +35,24 @@ function makeCard() {
     for (let i = 1; i <= 12; i++) {
         // div 생성
         const card = document.createElement("div");
-        const cardInner = document.createElement("div");
-        const cardFront = document.createElement("div");
-        const cardBack = document.createElement("div");
+        const cardinner = document.createElement("div");
+        const cardfront = document.createElement("div");
+        const cardback = document.createElement("div");
 
-        cardInner.classList.add('card-Inner');
-        cardFront.classList.add('card-Front');
-        cardBack.classList.add('card-Back');
+        cardinner.classList.add('card-inner');
+        cardfront.classList.add('card-front');
+        cardback.classList.add('card-back');
         card.classList.add('card');
 
         // 만들어진 카드들을 가상 돔에 입력
-        card.appendChild(cardInner);
-        cardInner.appendChild(cardFront);
-        cardInner.appendChild(cardBack);
+        card.appendChild(cardinner);
+        cardinner.appendChild(cardfront);
+        cardinner.appendChild(cardback);
         $frag.appendChild(card);
 
         //카드 색 적용
         card.style.backgroundColor = 'navy';
-        cardBack.style.backgroundColor = 'black';
+        cardback.style.backgroundColor = 'black';
 
         // 클릭시 flipped class 부여, 있으면 제거
         card.addEventListener("click", () => {
